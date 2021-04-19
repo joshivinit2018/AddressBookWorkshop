8b7e8f16c9ceb9b5ba5dbaa7f52de4c997d4746e
joshivinit2018 <joshivinit2018@gmailcom>
2021-04-19 09:33:03 +0530

    [vinit]: create differnt contact with many parameter

---
+++ src/main/java/com/bridgelabz/Contact.java
@@ -0,0 +1,37 @@
+package com.bridgelabz;
+
+public class Contact {
+    public String fristname;
+    public String lastname;
+    public String address;
+    public String city;
+    public String state;
+    public int zip;
+    public long phone;
+    public String email;
+
+    public Contact(String fristname,String lastname,String address,String city,String state,int zip,long phone,String email){
+        this.fristname = fristname;
+        this.lastname = lastname;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.email = email;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "fristname='" + fristname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip=" + zip +
+                ", phone=" + phone +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
